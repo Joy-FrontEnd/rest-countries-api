f54c2ef92fa69d93dfeb6638c7cf890dd1550cbc
Joy-FrontEnd <bmmuthonijoy@gmail.com>
2021-01-25 06:19:44 +0300

    Displayed countries

---
+++ src/Components/Countries.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react'
+const apiUrl = "https://restcountries.eu/rest/v2/all"
+
+const Countries = () => {
+  const [countries, setCountries] = useState([])
+
+  const fetchCountryData = async () => {
+      const response = await fetch(apiUrl)
+      const countries = await response.json()
+      setCountries(countries)
+      console.log(countries)  }
+
+
+  useEffect(() => {
+      fetchCountryData()
+    }, [])
+     
+  return (
+    <>
+   <section className="grid">
+      
+    {countries.map((country) =>{
+      const { numericCode, name, population, region, capital, flag } = country
+
+      return <article key={numericCode}>
+        <div>
+          <img src={flag} alt={name} />
+          <h3>{name}</h3>
+          <ul>
+            <li>Population: <span>{population}</span></li>
+            <li>Capital: <span>{capital}</span></li>
+            <li>Region: <span>{region}</span></li>
+          </ul>
+        </div>
+      </article>
+    })}
+      
+   
+   </section>
+
+    </>
+  )
+}
+export default Countries
+
+
